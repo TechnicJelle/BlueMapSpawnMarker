bd9ab1edc4d350b5127a0e101d8b015cf40c8149
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-03-28 17:56:03 +0100

    Use tabs

--- src/main/java/com/technicjelle/BlueMapSpawnMarker/LevelData.java
+++ src/main/java/com/technicjelle/BlueMapSpawnMarker/LevelData.java
@@ -8,63 +8,63 @@ import de.bluecolored.bluenbt.NBTName;
 @SuppressWarnings("FieldMayBeFinal")
 public class LevelData {
 
-    @NBTName("Data")
-    private Data data = new Data();
+	@NBTName("Data")
+	private Data data = new Data();
 
-    public Data getData() {
-        return data;
-    }
+	public Data getData() {
+		return data;
+	}
 
-    public static class Data {
+	public static class Data {
 
-        private Spawn spawn = null;
+		private Spawn spawn = null;
 
-        // legacy-spawn notation
-        @NBTName("SpawnX")
-        private int spawnX = 0;
+		// legacy-spawn notation
+		@NBTName("SpawnX")
+		private int spawnX = 0;
 
-        @NBTName("SpawnY")
-        private int spawnY = 0;
+		@NBTName("SpawnY")
+		private int spawnY = 0;
 
-        @NBTName("SpawnZ")
-        private int spawnZ = 0;
+		@NBTName("SpawnZ")
+		private int spawnZ = 0;
 
-        public Spawn getSpawn() {
-            if (spawn == null) {
-                spawn = new Spawn(new Vector3i(spawnX, spawnY, spawnZ));
-            }
-            return spawn;
-        }
+		public Spawn getSpawn() {
+			if (spawn == null) {
+				spawn = new Spawn(new Vector3i(spawnX, spawnY, spawnZ));
+			}
+			return spawn;
+		}
 
-    }
+	}
 
-    public static class Spawn {
+	public static class Spawn {
 
-        private Key dimension = DataPack.DIMENSION_OVERWORLD;
-        private Vector3i pos = Vector3i.ZERO;
-        private float yaw = 0;
-        private float pitch = 0;
+		private Key dimension = DataPack.DIMENSION_OVERWORLD;
+		private Vector3i pos = Vector3i.ZERO;
+		private float yaw = 0;
+		private float pitch = 0;
 
-        public Spawn(Vector3i pos) {
-            this.pos = pos;
-        }
+		public Spawn(Vector3i pos) {
+			this.pos = pos;
+		}
 
-        public Key getDimension() {
-            return dimension;
-        }
+		public Key getDimension() {
+			return dimension;
+		}
 
-        public Vector3i getPos() {
-            return pos;
-        }
+		public Vector3i getPos() {
+			return pos;
+		}
 
-        public float getYaw() {
-            return yaw;
-        }
+		public float getYaw() {
+			return yaw;
+		}
 
-        public float getPitch() {
-            return pitch;
-        }
+		public float getPitch() {
+			return pitch;
+		}
 
-    }
+	}
 
 }
